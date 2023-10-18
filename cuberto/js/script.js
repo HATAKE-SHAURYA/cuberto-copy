9f7f2d00ad13ee073a1e093f244794617705f62c
HATAKE-SHAURYA <146454683+HATAKE-SHAURYA@users.noreply.github.com>
2023-10-18 20:03:58 +0530

    Update script.js

--- cuberto/js/script.js
+++ cuberto/js/script.js
@@ -30,3 +30,54 @@ document.querySelector("#overlay2").addEventListener("mouseleave", function(dets
     document.querySelector("#elem2 img").style.opacity = 0
     cursor.style.opacity = 1
 })
+// by me
+var comment = document.querySelector("#comment")
+// comment.addEventListener("mousemove", function(){
+//     cursor.style.opacity = 0
+//     comment.style.scale = 1.8
+// })
+// comment.addEventListener("mouseleave", function(){
+//     cursor.style.opacity = 1
+//     comment.style.scale = 1
+// })
+comment.addEventListener("mousemove", function(){
+    comment.classList.toggle(".comment-grow")
+    comment.style.scale = 1.8
+    cursor.style.opacity = 0
+})
+comment.addEventListener("mouseleave", function(){
+    comment.classList.toggle(".comment-grow")
+    comment.style.scale = 1
+    cursor.style.opacity = 1
+})
+var Lnav = document.querySelector("#navL")
+var RnavH1 = document.querySelectorAll("#navR h1")
+var burger = document.querySelector("#hamburger")
+
+Lnav.addEventListener("mousemove", function(){
+    cursor.style.scale = 0
+    cursor.style.opacity = 0
+    cursor.style.transition = "all linear 0.2s"
+})
+Lnav.addEventListener("mouseleave", function(){
+    cursor.style.scale = 1
+    cursor.style.opacity = 1
+})
+RnavH1.addEventListener("mousemove", function(){
+    cursor.style.scale = 0
+    cursor.style.opacity = 0
+    cursor.style.transition = "all linear 0.2s"
+})
+RnavH1.addEventListener("mouseleave", function(){
+    cursor.style.scale = 1
+    cursor.style.opacity = 1
+})
+burger.addEventListener("mousemove", function(){
+    cursor.style.scale = 0
+    cursor.style.opacity = 0
+    cursor.style.transition = "all linear 0.2s"
+})
+burger.addEventListener("mouseleave", function(){
+    cursor.style.scale = 1
+    cursor.style.opacity = 1
+})
